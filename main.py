6fa0c6f6d7933425aa046d433fa4accc8a51f1ca
Bill Skarlatos <skarlatov@csd.auth.gr>
2024-11-04 22:40:47 +0200

    Data presentation

--- main.py
+++ main.py
@@ -9,4 +9,10 @@ def compare(NCC_acc, KNN_acc):
     else:
         print(f"Both methods had equal accuracies with: {NCC_acc * 100:.2f}%")
 
-compare(ncc.NCC("DB"),knn.KNN("DB",1))
+centroid=ncc.NCC("DB")
+knn1=knn.KNN("DB",1)
+knn3=knn.KNN("DB",3)
+print("\nFor 1st nearest neighbour:")
+compare(centroid,knn1)
+print("For the 3 nearest neighbours:")
+compare(centroid,knn3)
