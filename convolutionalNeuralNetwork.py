7770c1d16e456439494aa1fd1b26f1851f41f395
Bill Skarlatos <skarlatosbill@gmail.com>
2024-11-22 18:08:06 +0200

    Renaming

--- convolutionalNeuralNetwork.py
+++ convolutionalNeuralNetwork.py
@@ -3,9 +3,9 @@ import torch.optim as optim
 import dataHandling as dh
 
 # Define a simple CNN
-class SimpleCNN(nn.Module):
+class CNN(nn.Module):
     def __init__(self):
-        super(SimpleCNN, self).__init__()
+        super(CNN, self).__init__()
         self.conv1 = nn.Conv2d(3, 32, 3, padding=1)
         self.conv2 = nn.Conv2d(32, 64, 3, padding=1)
         self.pool = nn.MaxPool2d(2, 2)
@@ -22,7 +22,7 @@ class SimpleCNN(nn.Module):
         return x
 
 # Initialize the model, loss function, and optimizer
-model = SimpleCNN()
+model = CNN()
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=0.001)
 
